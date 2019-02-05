fbf1d0990fb04501fceeb595dafdc1b67a7646ee
Unknown <laerte.guimaraes@vindi.com.br>
2019-02-05 14:06:16 -0200

    Ajusta Ex 4

--- CodeSaga/Embarcando no vagão Rails/Indo além do básico/app/controllers/tasks_controller.rb
+++ CodeSaga/Embarcando no vagão Rails/Indo além do básico/app/controllers/tasks_controller.rb
@@ -1,16 +1,18 @@
 class TasksController < ApplicationController
   def new
     @task = Task.new
+    @categories = Category.all
   end
 
   def create
     # @task = Task.new(description: params[:task][:description], status: params[:task][:status])
     
     # evita códigos maliciosos
-    @task = Task.new(params.require(:task).permit(:description, :status))
+    @task = Task.new(params.require(:task).permit(:description, :status, :category_id))
     if @task.save # redireciona para root se for possível salvar
       redirect_to root_path
     else 
+      @categories = Category.all #disponibilizamos a variável para rodar o render
       render :new # retorna os erros na própria página de criação
     end
   end
@@ -18,15 +20,18 @@ class TasksController < ApplicationController
   def edit
     # utilizamos o params, dando uma olhada no routes vemos o nome do parâmetro na Rota de edit.
     @task = Task.find(params[:id])
+
+    @categories = Category.all
   end
 
   def update
     @task = Task.find(params[:id])
-    @task.update(params.require(:task).permit(:description, :status))
+    @task.update(params.require(:task).permit(:description, :status, :category_id))
 
     if @task.save # redireciona para root se for possível salvar
       redirect_to root_path
     else 
+      @categories = Category.all
       render :new # retorna os erros na própria página de criação
     end
   end
